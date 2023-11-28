43b9305a6931a9dae33fe3c428c95aeb089dd448
susantlalshrestha <susantlalshrestha@gmail.com>
2023-11-28 00:01:58 -0500

    Initial commit

---
+++ encript.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+async function encryptPassword(password) {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function comparePassword(password, encrpytedPassword) {
+  try {
+    return await bcrypt.compare(password, encrpytedPassword);
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { encryptPassword };

74609589c1f8bd218dfb6eac4403e5fbe3a62cc7
susantlalshrestha <susantlalshrestha@gmail.com>
2023-12-17 00:00:23 -0500

    made just few changes

---
+++ frontend/js/room.js
@@ -0,0 +1,9 @@
+function editRoom(room) {
+  // Add logic to handle the edit action
+  console.log(`Editing room with ID: ${roomId}`);
+}
+
+function deleteRoom(roomId) {
+  // Add logic to handle the delete action
+  console.log(`Deleting room with ID: ${roomId}`);
+}
